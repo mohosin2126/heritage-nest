deb4eb87d1d15ba9059c060507114fe3f82c83a6
Mohosin212601~ <mohosin2126@gmail.com>
2024-04-30 18:09:22 +0600

    created home component and banner and connected both and set a home route in router pages

---
+++ src/Home/Banner.jsx
@@ -0,0 +1,10 @@
+
+const Banner = () => {
+    return (
+        <div>
+            <h1>hello this is me home </h1>
+        </div>
+    );
+};
+
+export default Banner;
--- src/Routes/router.jsx
+++ src/Routes/router.jsx
@@ -2,10 +2,17 @@ import {
     createBrowserRouter
   } from "react-router-dom";
 import Layout from "../Layout/Layout";
+import Home from "../Home/Home";
   
   export const router = createBrowserRouter([
     {
       path: "/",
       element:<Layout></Layout>,
+      children:[
+        {
+            path:"/",
+            element:<Home></Home>
+        }
+      ]
     },
   ]);
