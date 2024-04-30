b67f0564db8942c9060ef35d41c31f671cc7d204
Mohosin212601~ <mohosin2126@gmail.com>
2024-04-30 22:15:02 +0600

    banner section done

--- src/Home/Banner.jsx
+++ src/Home/Banner.jsx
@@ -1,9 +1,28 @@
 
 const Banner = () => {
     return (
-        <div>
-            <h1>hello this is me home </h1>
-        </div>
+        <div >
+       <div className="hero h-96">
+  <video autoPlay loop muted>
+    <source src="../../public/-f8f8-4a60-bda2-7982b8d7a898.mp4" type="video/mp4" />
+    Your browser does not support the video tag.
+  </video>
+  <div className="hero-overlay bg-opacity-60">
+   <h1 className="text-center mt-52 text-white">Seamlessly connecting you to the heartbeat of India's prime properties.</h1>
+   <h1 className="text-center text-white mt-7 font-extrabold text-4xl">
+   Your Portal to India's <br />Exquisite Real Estate
+   </h1>
+   <div className="flex gap-3 w-36 mx-auto mt-6 p-1  bg-blue-500">
+    <img src="https://i.ibb.co/QJ7mZpC/Magnifer.png" alt="" />
+    <h1>Find Property
+</h1>
+   </div>
+  </div>
+  
+</div>
+
+      </div>
+      
     );
 };
 
