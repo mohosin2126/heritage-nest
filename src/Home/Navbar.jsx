2ea9baae43f1ae6c9fd0f2184b296f70ea4c1dcb
Mohosin212601~ <mohosin2126@gmail.com>
2024-04-30 18:03:20 +0600

    navbar done

--- src/Home/Navbar.jsx
+++ src/Home/Navbar.jsx
@@ -2,7 +2,24 @@
 const Navbar = () => {
     return (
         <div>
-          
+          <div className="navbar p-6 h-20 bg-base-100">
+  <div className="navbar-start">
+    <div className="flex gap-8 items-center font-normal">
+        <h1>Buy</h1>
+        <h1>Sell</h1>
+        <h1>Services</h1>
+    </div>
+  </div>
+  <div className="navbar-center">
+    <img className="h-20 w-12" src="https://i.ibb.co/8PN85zP/e8cbbb0006f4af5b4764c7061ba11f37.png" alt="" />
+  </div>
+  <div className="navbar-end">
+    <div className="flex items-center gap-8 font-normal">
+    <h1>Manage Rentals</h1>
+    <h1>Sign In </h1>
+    </div>
+  </div>
+</div>
         </div>
     );
 };
