baa90dbf11b00ded26359a14291905fe54c336e8
Mohosin212601~ <mohosin2126@gmail.com>
2024-05-01 17:25:03 +0600

    Banner Done

--- src/Home/Banner.jsx
+++ src/Home/Banner.jsx
@@ -1,31 +1,25 @@
 
 const Banner = () => {
-    return (
-        <div >
-       <div className="hero">
- <div  >
- <video autoPlay loop muted width="full" height="600">
-    <source src="../../public/-f8f8-4a60-bda2-7982b8d7a898.mp4" type="video/mp4" />
-    Your browser does not support the video tag.
-  </video>
- </div>
-  <div className="hero-overlay bg-opacity-60">
-   <h1 className="text-center mt-52 text-white">Seamlessly connecting you to the heartbeat of India's prime properties.</h1>
-   <h1 className="text-center text-white mt-7 font-extrabold text-4xl">
-   Your Portal to India's <br />Exquisite Real Estate
-   </h1>
-   <div className="flex gap-3 w-36 mx-auto mt-6 p-1  bg-blue-500">
-    <img src="https://i.ibb.co/QJ7mZpC/Magnifer.png" alt="" />
-    <h1>Find Property
-</h1>
-   </div>
-  </div>
-  
-</div>
+  return (
+    <div>
 
+      <div className="hero h-[70vh]" style={{ position: "relative", overflow: "hidden" }}>
+        <video autoPlay muted loop style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", objectFit: "cover", height: "100%", width: "100%", zIndex: -1 }}>
+          <source src="../../public/-f8f8-4a60-bda2-7982b8d7a898.mp4" type="video/mp4" />
+        </video>
+        <div className="hero-overlay bg-black bg-opacity-65"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-3xl text-white p-2">
+            <p>Seamlessly connecting you to the heartbeat of India's prime properties.</p>
+            <h1 className=" text-5xl font-bold my-10">Your Portal to India's <br />Exquisite Real Estate</h1>
+
+            <button className="btn btn-error text-lg border-0 text-white hover:text-black   bg-blue-500 "> <img src="https://i.ibb.co/QJ7mZpC/Magnifer.png" alt="" />Find Property</button>
+          </div>
+        </div>
       </div>
-      
-    );
+    </div>
+
+  );
 };
 
 export default Banner;
